c5b92e3b5192391bf527d71aa72e972cbe7aba9f
Oleg <olego250@gmail.com>
2022-02-03 17:18:30 +0200

    add: use-match-media-hook

--- src/components/UI/Loader/SkeletonList.js
+++ src/components/UI/Loader/SkeletonList.js
@@ -1,16 +1,32 @@
 import React from 'react';
+import useMatchMedia from 'use-match-media-hook'
 import SkeletonUiCard from './SkeletonUiCard';
 
+
+const queries = [
+    '(max-width: 480px)'
+]
+
 const SkeletonList = () => {
-    return (
-        <>
-            <SkeletonUiCard/>
-            <SkeletonUiCard/>
-            <SkeletonUiCard/>
-            <SkeletonUiCard/>
-            <SkeletonUiCard/>
-            <SkeletonUiCard/>
-        </>
+
+    const [mobile] = useMatchMedia(queries)
+
+    return (!mobile ?
+            (
+                <>
+                    <SkeletonUiCard/>
+                    <SkeletonUiCard/>
+                    <SkeletonUiCard/>
+                    <SkeletonUiCard/>
+                    <SkeletonUiCard/>
+                    <SkeletonUiCard/>
+                </>
+            ) :
+            (
+                <>
+                    <SkeletonUiCard/>
+                </>
+            )
     );
 };
 
